cbbe80e0da6a0e860d198cf763b0756053fa5126
Maryna Snitko <maryna.snitko@gmail.com>
2019-03-13 20:11:03 +0200

    Add project files.

---
+++ SeleniumWebDriver/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumWebDriver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumWebDriver")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93970885-9857-4bc6-9f07-911935f9c4dd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumWebDriver/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumWebDriver
+{
+    public class UITests
+    {
+        [Test]
+        public void TestWebDriverMethods()
+        {
+            IWebDriver driver = new ChromeDriver();
+        }
+    }
+}
